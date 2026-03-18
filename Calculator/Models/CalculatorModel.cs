c7d26128809b15489c8c00b5c5d5f2077fdfde3d
useress <useress2023@gmail.com>
2026-03-18 09:44:48 +0300

    base project structure

---
+++ Calculator/Models/CalculatorModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Calculator.Models
+{
+    public class CalculatorModel
+    {
+        public double Calculate(double a, double b, string op)
+        {
+            switch (op)
+            {
+                case "+":
+                    return a + b;
+
+                case "-":
+                    return a - b;
+
+                case "*":
+                    return a * b;
+
+                case "/":
+                    if (b == 0)
+                        throw new DivideByZeroException();
+                    return a / b;
+
+                default:
+                    return 0;
+            }
+        }
+    }
+}
---
+++ Calculator/ViewModels/CalculatorViewModel.cs
@@ -0,0 +1,65 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using Calculator.Models;
+
+namespace Calculator.ViewModels
+{
+    public class CalculatorViewModel : INotifyPropertyChanged
+    {
+        private readonly CalculatorModel _model = new CalculatorModel();
+
+        private string _number1;
+        private string _number2;
+        private string _result;
+        private string _operation;
+
+        public string Number1
+        {
+            get => _number1;
+            set { _number1 = value; OnPropertyChanged(); }
+        }
+
+        public string Number2
+        {
+            get => _number2;
+            set { _number2 = value; OnPropertyChanged(); }
+        }
+
+        public string Result
+        {
+            get => _result;
+            set { _result = value; OnPropertyChanged(); }
+        }
+
+        public string Operation
+        {
+            get => _operation;
+            set { _operation = value; OnPropertyChanged(); }
+        }
+
+        public ICommand CalculateCommand { get; }
+
+        public CalculatorViewModel()
+        {
+            CalculateCommand = new RelayCommand(Calculate);
+        }
+
+        private void Calculate(object obj)
+        {
+            double a = double.Parse(Number1);
+            double b = double.Parse(Number2);
+
+            double res = _model.Calculate(a, b, Operation);
+
+            Result = res.ToString();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
